e079e03ba7e8969789d9c93d2463ad4605afb83d
Charles Yang <yarlescy@gmail.com>
2022-06-18 22:22:06 -0500

    to html

--- frontend/data_stuff/distribution_factory.py
+++ frontend/data_stuff/distribution_factory.py
@@ -1,11 +1,13 @@
 import random
 from scipy.stats import norm
 import plotly.figure_factory as ff
+from plotly import io
 
 
 def normal(n):
     x = norm.rvs(size=n, loc=random.randint(-10, 10), scale=1)
-    return ff.create_distplot(x, show_hist=False)
+    fig = ff.create_distplot([x], show_hist=False)
+    return io.to_html(fig, include_plotlyjs=False)
 
 
     

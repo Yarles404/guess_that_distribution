3b7c5e992aa85f915eaf18671cf7dd847282e0f4
Charles Yang <yarlescy@gmail.com>
2022-06-18 22:12:27 -0500

    did normal distribution

--- frontend/data_stuff/distribution_factory.py
+++ frontend/data_stuff/distribution_factory.py
@@ -1,1 +1,13 @@
 import random
+from scipy.stats import norm
+import plotly.figure_factory as ff
+
+
+def normal(n):
+    x = norm.rvs(size=n, loc=random.randint(-10, 10), scale=1)
+    return ff.create_distplot(x, show_hist=False)
+
+
+    
+
+

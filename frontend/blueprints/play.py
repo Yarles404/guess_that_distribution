bf33a6b65168a0ef47ba98970fff84c26e3021cf
Charles Yang <yarlescy@gmail.com>
2022-07-10 20:26:06 -0500

    get & display multiple choice options

--- frontend/blueprints/play.py
+++ frontend/blueprints/play.py
@@ -1,22 +1,32 @@
 from flask import Blueprint, render_template
 import random
-from data_stuff.distribution_factory import normal
+from data_stuff.distribution_factory import normal, normal1, normal2, normal3
 
 
 play = Blueprint('play', __name__)
 
-
-DISTRIBUTIONS = [
-    normal
-]
+DISTRIBUTIONS = {
+#   'name': distribution_function
+    'Normal': normal,
+    'Normal1': normal1,
+    'Normal2': normal2,
+    'Normal3': normal3,
+}
 
 
 def get_random_distribution():
-    return DISTRIBUTIONS[random.randint(0, len(DISTRIBUTIONS) - 1)](100) # Change to n
+    name, distribution_function = random.choice(list(DISTRIBUTIONS.items()))
+    return distribution_function(100), name # Change to n instead of 100
 
+def get_random_options(correct_option):
+    distribution_names = list(DISTRIBUTIONS.keys())
+    distribution_names.remove(correct_option)
+    wrong_options = random.sample(distribution_names, 3)
+    return [correct_option] + wrong_options
 
 @play.route('/play', methods=['GET'])
 @play.route('/', methods=['GET'])
 def question():
-    distribution = get_random_distribution()
-    return render_template('play.html', distribution=distribution)
+    distribution, correct_option = get_random_distribution()
+    answer_options = get_random_options(correct_option)
+    return render_template('play.html', distribution=distribution, answer_options=answer_options)
--- frontend/data_stuff/distribution_factory.py
+++ frontend/data_stuff/distribution_factory.py
@@ -3,17 +3,26 @@ from scipy.stats import norm
 import plotly.figure_factory as ff
 from plotly import io
 
-
 DIV_ID = 'guess-me'
-
 
 def distribution_plot(data):
     fig = ff.create_distplot([data], ['A Mysterious Distribution'], show_hist=False)
     return io.to_html(fig, include_plotlyjs=False, div_id=DIV_ID, full_html=False)
 
-
 def normal(n):
     x = norm.rvs(size=n, loc=random.randint(-10, 10), scale=1)
+    return distribution_plot(x)
+
+def normal1(n):
+    x = norm.rvs(size=n, loc=random.randint(-10, 10), scale=2)
+    return distribution_plot(x)
+
+def normal2(n):
+    x = norm.rvs(size=n, loc=random.randint(-10, 10), scale=3)
+    return distribution_plot(x)
+
+def normal3(n):
+    x = norm.rvs(size=n, loc=random.randint(-10, 10), scale=4)
     return distribution_plot(x)
 
 

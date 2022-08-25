2db7af6105a6fd29e28a2c39ec01694d123e8afc
Charles Yang <yarlescy@gmail.com>
2022-08-25 11:24:38 -0500

    play model

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
+from app import db
 
-class Play()
+class Play(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    player_id = db.Column(db.String, nullable=False)
+    correct_answer = db.Column(db.String, nullable=False)
+    player_answer = db.Column(db.String, nullable=False)
+    correct = db.Column(db.Boolean, nullable=False)
